cfec5875d8f12b4deeec592ae1d2594e45d5f51a
hemalatha0512 <shemalatha@student.tce.edu>
2026-02-07 12:43:18 +0530

    Update data_quality_checker.py

--- data_quality_checker.py
+++ data_quality_checker.py
@@ -5,13 +5,13 @@ print("Automated CSV Data Quality Checker")
 print("----------------------------------")
 
 if len(sys.argv) < 2:
-    print("Usage: data_quality_checker.py sales_data.csv ")
+    print("Usage: data_quality_checker.py <csv_file>")
     sys.exit(1)
 
 file_name = sys.argv[1]
 
 try:
-    df = pd.read_csv(sales_data.csv)
+    df = pd.read_csv(file_name)
 except FileNotFoundError:
     print("Error: File not found")
     sys.exit(1)
@@ -33,4 +33,3 @@ if missing_values == 0 and duplicate_rows == 0 and negative_values == 0:
     print("Data Quality Status: PASS")
 else:
     print("Data Quality Status: FAIL")
-
